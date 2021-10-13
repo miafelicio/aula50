911c32979f562a9917ab9fad5452d8f6bb9a9dc6
Belle-Elle <>
2021-10-13 11:22:42 -0300

    aniversario

---
+++ Aula50.js
@@ -0,0 +1,56 @@
+// Declarar uma variável qualquer, que receba um objeto vazio.
+var pessoa = new Object
+
+/*
+Declarar uma variável `pessoa`, que receba suas informações pessoais.
+As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
+- `nome` - String
+- `sobrenome` - String
+- `sexo` - String
+- `idade` - Number
+- `altura` - Number
+- `peso` - Number
+- `andando` - Boolean - recebe "falso" por padrão
+- `caminhouQuantosMetros` - Number - recebe "zero" por padrão
+*/
+
+
+
+pessoa.nome = 'angolano',
+pessoa.sobrenome = 'ano',
+pessoa.sexo = 'trevas',
+pessoa.idade = 16,
+pessoa.altura = 1.32,
+pessoa.peso = 33,
+pessoa.andando = false,
+pessoa.caminhouMetros = 0
+
+
+
+/*
+Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
+alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
+for chamado.
+*/
+
+pessoa.fazerAniversario = function()
+{
+pessoa.idade++
+}
+
+pessoa.fazerAniversario()
+console.log(pessoa.idade);
+
+
+/*
+Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
+características:
+- Esse método deve receber por parâmetro um valor que representará a quantidade
+de metros caminhados;
+- Ele deve alterar o valor da propriedade `caminhouQuantosMetros`, somando ao
+valor dessa propriedade a quantidade passada por parâmetro;
+- Ele deverá modificar o valor da propriedade `andando` para o valor
+booleano que representa "verdadeiro";
+*/
+
+
